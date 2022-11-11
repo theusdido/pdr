866f2a62093249379bbcf913661799546b9f5d1b
theusdido <edilsonbitencourt@hotmail.com>
2022-11-11 16:45:21 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroEmpresaComponent } from './cadastro-empresa/cadastro-empresa.component';
+import { InicialComponent } from './inicial/inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home' , component:InicialComponent},
+  {path:'cadastroempresa', component:CadastroEmpresaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './inicial/inicial.component';
+import { CadastroEmpresaComponent } from './cadastro-empresa/cadastro-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    CadastroEmpresaComponent
   ],
   imports: [
     BrowserModule,
